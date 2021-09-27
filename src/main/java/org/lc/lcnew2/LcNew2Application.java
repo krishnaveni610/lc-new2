bb22bcdf3025b879914e5777c65fcdf1aaa4f741
krishnaveni610 <krishnaveni610@gmail.com>
2021-09-27 15:54:28 +0530

    Add files via upload

---
+++ src/main/java/org/lc/lcnew2/LcNew2Application.java
@@ -0,0 +1,13 @@
+package org.lc.lcnew2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcNew2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LcNew2Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/lc/lcnew2/LcNew2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lc.lcnew2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LcNew2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
